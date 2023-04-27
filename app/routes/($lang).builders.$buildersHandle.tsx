9f221423ac2ddec7da3533243a9644c03de5d779
Hau Nguyen <Hau.Nguyen@isobar.com>
2023-04-27 10:41:58 +0700

    init package 3

--- app/routes/($lang).builders.$buildersHandle.tsx
+++ app/routes/($lang).builders.$buildersHandle.tsx
@@ -1,51 +1,40 @@
-import {json, type LoaderArgs} from '@shopify/remix-oxygen';
-import type {Page as PageType} from '@shopify/hydrogen/storefront-api-types';
-import {useLoaderData} from '@remix-run/react';
-import invariant from 'tiny-invariant';
-import {PageHeader} from '~/components';
-import {CACHE_LONG, routeHeaders} from '~/data/cache';
-import {seoPayload} from '~/lib/seo.server';
-import {BuilderComponent, builder} from '@builder.io/react';
-import {Builder} from '@builder.io/sdk';
+import {builder, BuilderComponent, BuilderContent} from '@builder.io/react';
+import {Await, useLoaderData} from '@remix-run/react';
+import {Suspense} from 'react';
 builder.init('0eed6b8cdef047658652879b5de72b72');
-export const headers = routeHeaders;
 
-export async function loader({request, params, context}: LoaderArgs) {
-  console.log('test builder');
-  const pathname = '/builder/testing';
+const BUILDER_MODEL = 'page';
 
-  // Fetch data content from Builder.io based on the URL path
-  const content = await builder
-    .get('page', {
+export async function loader() {
+  const builderContent = await builder
+    .get(BUILDER_MODEL, {
       userAttributes: {
-        urlPath: pathname,
+        urlPath: '/builder/testing',
       },
     })
     .toPromise();
-  //console.log(content);
-  const seo = null;
 
-  return json(
-    {seo, content},
-    {
-      headers: {
-        'Cache-Control': CACHE_LONG,
-      },
-    },
-  );
+  if (!builderContent) {
+    throw new Response('Not found', {status: 404});
+  }
+
+  return builderContent;
 }
 
+// this gives full compatibility to BuilderContent type and Remix starter
+// See: https://github.com/BuilderIO/builder/issues/1387#issuecomment-1397442797
+type BuilderContentRemix = Omit<BuilderContent, 'variations' | 'data'>;
+
 export default function Builders() {
-  const {content} = useLoaderData<typeof loader>();
-  console.log(content);
+  console.log('test abc');
+  const builderContent: BuilderContentRemix =
+    useLoaderData<BuilderContentRemix>();
+
   return (
-    <>
-      <PageHeader heading="Test builder">
-        <div>
-          <h2>Testing builder.io</h2>
-          <BuilderComponent model="page" content={content} />
-        </div>
-      </PageHeader>
-    </>
+    <Suspense>
+      <Await resolve={builderContent}>
+        <BuilderComponent content={builderContent} model={BUILDER_MODEL} />
+      </Await>
+    </Suspense>
   );
 }
--- server.ts
+++ server.ts
@@ -38,7 +38,7 @@ export default {
         cache,
         waitUntil,
         i18n: getLocaleFromRequest(request),
-        publicStorefrontToken: '4b4cd2574d9eee76a290b3333d57d3d5',
+        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
         privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
         storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
         storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-04',
