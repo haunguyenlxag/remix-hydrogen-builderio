966e39c55a7d062664116181c1afc01820f0f70f
Hau Nguyen <Hau.Nguyen@isobar.com>
2023-04-28 07:54:31 +0700

    builderio

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,7 @@ module.exports = {
   appDirectory: 'app',
   ignoredRouteFiles: ['**/.*'],
   watchPaths: ['./public'],
-  server: './server.ts',
+  server: './server.js',
   /**
    * The following settings are required to deploy Hydrogen apps to Oxygen:
    */
